e80e7049b76329d87968ae0fdcd908c1798a5a9e
Adel Kandi <adelkandi68@gmail.com>
2023-10-01 21:17:36 +0100

    New Update

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import os
 import requests
 import discord
 import openai   # Using open ai Gpt 3  as helper 
+import io 
+
+# Ban_Exterminator : 
 
 
-
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
